c2dcb088a4966586f42506831d5b4740b87a2457
Lukáš Vaněk <lukas@sinesquare.com>
2020-10-01 13:43:57 +0200

    Added pages

---
+++ src/pages/Post.tsx
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+const Post = () => {
+  return <div></div>;
+};
+
+export default Post;
---
+++ src/pages/Posts.tsx
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+const Posts = () => {
+  return <div></div>;
+};
+
+export default Posts;
