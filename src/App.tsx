1daa535f9c423512be2b7c4ebe462280703db2cf
Lukáš Vaněk <lukas@sinesquare.com>
2020-10-02 13:01:14 +0200

    ScrollIntoView fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,26 @@ import Post from './pages/Post';
 import Posts from './pages/Posts';
 import User from './pages/User';
 import Header from './components/Header';
+import ScrollIntoView from './components/ScrollIntoView';
 
 function App() {
   return (
     <div sx={{ variant: 'styles' }}>
       <Router>
         <Header />
-        <Switch>
-          <Route path="/user/:id">
-            <User />
-          </Route>
-          <Route path="/post/:id">
-            <Post />
-          </Route>
-          <Route path="/">
-            <Posts />
-          </Route>
-        </Switch>
+        <ScrollIntoView>
+          <Switch>
+            <Route path="/user/:id">
+              <User />
+            </Route>
+            <Route path="/post/:id">
+              <Post />
+            </Route>
+            <Route path="/">
+              <Posts />
+            </Route>
+          </Switch>
+        </ScrollIntoView>
       </Router>
     </div>
   );
